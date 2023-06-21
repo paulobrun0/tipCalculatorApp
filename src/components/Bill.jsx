f253966d01338c3e7924558ff35c3350f1ac8531
paulobrun0 <paulo.bruno04@hotmail.com>
2023-06-20 22:18:54 -0300

    Projeto finalizado

--- src/components/Bill.jsx
+++ src/components/Bill.jsx
@@ -4,35 +4,43 @@ import { ButtonTip } from './ButtonTip'
 import { BillResult } from './BillResult'
 import { useState } from 'react'
 export const Bill = () => {
-  const [billInput, setBillInput] = useState('')
-  const [totalPersonInput, setTotalPersonInput] = useState('')
+  const [billInput, setBillInput] = useState(100)
+  const [totalPersonInput, setTotalPersonInput] = useState(2)
+  const [customTip, setCustomTip] = useState('')
   const [tipAmountPerPerson, setTipAmountPerPerson] = useState(0.0)
   const [totalPersonBill, setTotalPersonBill] = useState(0.0)
-  const [customTip, setCustomTip] = useState('')
+  const [billInputCheck, setBillInputCheck] = useState(false)
+  const [totalPersonInputCheck, setTotalPersonInputCheck] = useState(false)
 
   const tipsButton = [5, 10, 15, 25, 50]
 
   const calcBill = (e, tipLabel) => {
     e.preventDefault()
 
-    const billFloat = +billInput
-    const totalPersonFloat = +totalPersonInput
-    const customTipFloat = +customTip
-    const tipValue = (billFloat * tipLabel) / 100
-    const tipValueCustom = (billFloat * customTipFloat) / 100
-    const totalBillPerPerson = billFloat / totalPersonFloat + tipValue
-    const totalBillPerPersonCustom =
-      billFloat / totalPersonFloat + customTipFloat
-    setTipAmountPerPerson(tipValue)
-    setTotalPersonBill(totalBillPerPerson)
+    const billFloat = parseFloat(billInput)
+    const totalPersonFloat = parseFloat(totalPersonInput)
 
-    handleKeyDown(e, tipValueCustom, totalBillPerPersonCustom)
+    if (!billFloat && !totalPersonFloat) {
+      setBillInputCheck(true)
+      setTotalPersonInputCheck(true)
+    } else if (!billFloat || billFloat < 0) {
+      setBillInputCheck(true)
+      return
+    } else if (!totalPersonFloat || totalPersonFloat < 0) {
+      setTotalPersonInputCheck(true)
+      return
+    } else {
+      const tipValue = (billFloat * tipLabel) / 100 / 2
+      const totalBillPerPerson = billFloat / totalPersonFloat + tipValue
+
+      setTipAmountPerPerson(tipValue)
+      setTotalPersonBill(totalBillPerPerson)
+    }
   }
 
-  const handleKeyDown = (e, tipValueCustom, totalBillPerPersonCustom) => {
+  const handleKeyDown = (e) => {
     if (e.key === 'Enter') {
-      setTipAmountPerPerson(tipValueCustom)
-      setTotalPersonBill(totalBillPerPersonCustom)
+      calcBill(e, parseFloat(customTip))
     }
   }
 
@@ -42,28 +50,56 @@ export const Bill = () => {
     setBillInput('')
     setTotalPersonInput('')
     setCustomTip('')
+    setBillInputCheck(false)
+    setTotalPersonInputCheck(false)
+  }
+
+  const handleBillInputChange = (e) => {
+    setBillInput(e.target.value)
+    setBillInputCheck(false)
+  }
+
+  const handleTotalPersonInputChange = (e) => {
+    setTotalPersonInput(e.target.value)
+    setTotalPersonInputCheck(false)
+  }
+
+  const handleCustomTip = (e) => {
+    setCustomTip(e.target.value)
+    console.log(e.target.value)
   }
 
   return (
     <div className={styles.containerBill}>
       <form className={styles.form}>
         <label className={styles.label}>
-          Bill
+          <div className={styles.labelContent}>
+            Bill
+            {billInputCheck && (
+              <span className={styles.check}>Can't be zero or less</span>
+            )}
+          </div>
+
           <CurrencyDollar size={24} />
           <input
-            className={styles.input}
+            className={billInputCheck ? styles.checkInput : styles.input}
             type="number"
             placeholder="0"
             value={billInput}
-            onChange={(e) => setBillInput(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                e.preventDefault()
+              }
+            }}
+            onChange={handleBillInputChange}
           />
         </label>
         <div className={styles.containerTips}>
           <label className={styles.label}>Select Tip %</label>
           <ul>
-            {tipsButton.map((tipsButton) => (
-              <li className={styles.list} key={tipsButton}>
-                <ButtonTip tipLabel={tipsButton} calcBill={calcBill} />
+            {tipsButton.map((tip) => (
+              <li className={styles.list} key={tip}>
+                <ButtonTip tipLabel={tip} calcBill={calcBill} />
               </li>
             ))}
             <li className={styles.list}>
@@ -73,21 +109,32 @@ export const Bill = () => {
                 placeholder="Custom"
                 value={customTip}
                 onKeyDown={handleKeyDown}
-                onChange={(e) => setCustomTip(e.target.value)}
+                onChange={handleCustomTip}
               />
             </li>
           </ul>
         </div>
 
         <label className={styles.label}>
-          Number of People
+          <div className={styles.labelContent}>
+            Number of People
+            {totalPersonInputCheck && (
+              <span className={styles.check}>Can't be zero or less</span>
+            )}
+          </div>
+
           <User size={24} />
           <input
-            className={styles.input}
+            className={totalPersonInputCheck ? styles.checkInput : styles.input}
             type="number"
             placeholder="0"
             value={totalPersonInput}
-            onChange={(e) => setTotalPersonInput(e.target.value)}
+            onChange={handleTotalPersonInputChange}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                e.preventDefault()
+              }
+            }}
           />
         </label>
       </form>
